43bc35e7d5c7dd87ef4df2a74a815118344f0f75
zakaria79 <othmane.zakaria79@gmail.com>
2017-12-05 08:54:32 +0100

    Avancement dans la parie formulaire depuis le portable

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -10,6 +10,7 @@ const input = props => {
         className={classes.InputElement}
         {...props.elementConfig}
         value={props.value}
+        onChange={props.changed}
       />;
       break;
     case 'textarea':
@@ -17,12 +18,32 @@ const input = props => {
         className={classes.InputElement}
         {...props.elementConfig}
         value={props.value}
+        onChange={props.changed}
       />;
+    break;
+    case 'select':
+      inputElement = (
+      <select
+        className={classes.InputElement}
+        value={props.value}
+        onChange={props.changed}
+      >
+        {props.elementConfig.options.map(option => (
+          <option
+            key={option.value}
+            value={option.value}>
+            {option.displayValue}
+            </option>
+        ))}
+      </select>
+      );
+    break;
     default:
       inputElement = <input
         className={classes.InputElement}
         {...props.elementConfig}
         value={props.value}
+        onChange={props.changed}
       />;
   }
 
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -65,9 +65,14 @@ class ContactData extends Component {
     orderHandler = ( event ) => {
         event.preventDefault();
         this.setState( { loading: true } );
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+          formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
+            orderData: formData
         }
         axios.post( '/orders.json', order )
             .then( response => {
@@ -79,14 +84,38 @@ class ContactData extends Component {
             } );
     }
 
+    inputChangedHandler = (event, inputIdentifier) => {
+      const updatedOrderForm = {
+        ...this.state.orderForm
+      };
+      const updatedFormElement = {
+        ...updatedOrderForm[inputIdentifier]
+      };
+      updatedFormElement.value = event.target.value;
+      updatedOrderForm[inputIdentifier] = updatedFormElement;
+      this.setState({orderForm: updatedOrderForm});
+    }
+
     render () {
+      const formElementsArray = [];
+      for (let key in this.state.orderForm) {
+        formElementsArray.push({
+          id: key,
+          config: this.state.orderForm[key]
+        });
+      }
         let form = (
-            <form>
-              <Input elementType="..." elementConfig="" value="..." />
-              <Input inputtype="input" type="email" name="email" placeholder="Your Mail" />
-              <Input inputtype="input" type="text" name="street" placeholder="Street" />
-              <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" />
-              <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            <form onSubmit={this.orderHandler}>
+              {formElementsArray.map(formElement => (
+                <Input
+                  key={formElement.id}
+                  elementType={formElement.config.elementType}
+                  elementConfig={formElement.config.elementConfig}
+                  value={formElement.config.value}
+                  changed={(event) => this.inputChangedHandler(event, formElement.id)}
+                />
+              ))}
+              <Button btnType="Success" >ORDER</Button>
             </form>
         );
         if ( this.state.loading ) {
