3523d63bcf74eb563a340539ff079c57e0dc1757
zakaria <othmane.zakaria79@gmail.com>
2017-12-08 22:09:33 +0100

    Après redux

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,12 +5,12 @@ import classes from './BuildControl.css';
 const buildControl = (props) => (
     <div className={classes.BuildControl}>
         <div className={classes.Label}>{props.label}</div>
-        <button 
-            className={classes.Less} 
-            onClick={props.removed} 
+        <button
+            className={classes.Less}
+            onClick={props.removed}
             disabled={props.disabled}>Less</button>
-        <button 
-            className={classes.More} 
+        <button
+            className={classes.More}
             onClick={props.added}>More</button>
     </div>
 );
